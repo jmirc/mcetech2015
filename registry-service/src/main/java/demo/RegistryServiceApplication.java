8ee434ea4a9d299a8df909d913fe90114809bdf4
Jérôme Mirc <jerome.mirc@gmail.com>
2015-05-08 09:19:58 -0400

    Add support to docker

--- registry-service/src/main/java/demo/RegistryServiceApplication.java
+++ registry-service/src/main/java/demo/RegistryServiceApplication.java
@@ -1,14 +1,20 @@
 package demo;
 
+import com.netflix.config.ConfigurationManager;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegistryServiceApplication.class, args);
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(RegistryServiceApplication.class, args);
+
+        ConfigurationManager.getDeploymentContext().setDeploymentEnvironment(StringUtils.join(applicationContext.getEnvironment().getActiveProfiles(), ","));
+        ConfigurationManager.getDeploymentContext().setDeploymentDatacenter("local");
     }
 }
